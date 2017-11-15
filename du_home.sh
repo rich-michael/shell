f35ac1cc953850e0d8bec400b5d3caa263370bb9
michael_tang <690806184@qq.com>
2017-11-15 15:49:13 +0800

    Update du_home.sh
    修改了两个地方，第一个是由于每台服务器分区不一样，导致可能查找不了磁盘的总容量，第二个，是增加了邮件提醒功能

--- du_home.sh
+++ du_home.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
 
-#查看哪个网卡在流通数据,
-#查看对应的网关IP
-#em=`route -n| grep "192.168.1.1" | awk '{print $8}' `
-#ip=`ifconfig $em | grep "192.168.1." |awk '{print $2}'|awk -F: '{print $2}'`
-
 #遍历/home,得出容量与用户，导出到对应文件/tmp/du.log
 for i in /home/*
    do
        [ -d  $i ] && du -sh $i
 
-   done | sed -n 's/\/home\///gp' > /home/prize-bs2/du.log
+   done | sed -n 's/\/home\///gp' > $HOME/du.log
 
 sleep 2
-#机器内部用量
-space_sum=`df -h|awk 'NR==10{print $2}'`
-space_used=`df -h|awk 'NR==10{print $3}'`
-space_left=`df -h|awk 'NR==10{print $4}'`
-used_percent=`df -h|awk 'NR==10{print $5}'`
+#机器内部用量，space_sum总容量，space_used已使用量，space_left剩余用量，used_percent百分比
+space_sum=`df -h|grep "T"|awk '{print $2}'`
+space_used=`df -h|grep "T"|awk '{print $3}'`
+space_left=`df -h|grep "T"|awk '{print $4}'`
+used_percent=`df -h|grep "T"|awk '{print $5}'`
 
 #测试环境
 key="project_auto_check20!*"
@@ -26,7 +21,7 @@ t=`curl -s "$url/PJset.php?type=102"`
 sign=`echo -n "$t$key"|md5sum|cut -d ' ' -f1`
 
 #正式环境
-net_url="http://pj.szprize.cn"
+net_url="xxxx.cn"
 net_t=`curl -s "$net_url/PJset.php?type=102"`
 net_sign=`echo -n "$net_t$key"|md5sum|cut -d ' ' -f1`
 
@@ -41,7 +36,18 @@ while read line
                 space=`echo "$line" |awk '{print $1}'`
                 curl -s "$url/PJset.php?type=66&op=setUserSpace&user=$user&space=$space&ip=$HOSTNAME&t=$t&sign=$sign"
                 curl -s "$net_url/PJset.php?type=66&op=setUserSpace&user=$user&space=$space&ip=$HOSTNAME&t=$net_t&sign=$net_sign"
-        done < /home/prize-bs2/du.log
+		T_space=`echo $space|grep "T"`
+		if [ -z $T_space ];then
+        		continue
+		else
+        		T_num=`echo $T_space|sed 's/T//g'`
+        		c=`echo "$T_num > 1.5"|bc`
+        		if [ "$c" -eq "1" ];then
+        		echo "你使用的服务器空间已超过1.5T，请及时清理"|mail -s "服务器空间监控"  $user@szprize.com
+        		echo "你使用的服务器空间已超过1.5T，请及时清理"|mail -s "服务器空间监控"  $user@szprize.com
+        		fi
+		fi
+	done < $HOME/du.log
 
 #查看结果
-[ $? -eq 0 ] && echo "$(date),脚本执行OK" >> /home/prize-bs2/result.log
+[ $? -eq 0 ] && echo "$(date),脚本执行OK" >> $HOME/result.log
